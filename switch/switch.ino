5290c2e6eadb796582df5b3fb429964f386f28cc
hyoje099 <hyoje099@naver.com>
2022-07-17 18:52:50 +0900

    저항

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(13,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(13)==HIGH){
+    Serial.println("1");
+  }else if(digitalRead(13)==LOW){
+    Serial.println("0");
+  }
 }
