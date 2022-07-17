3f75995d8294399663b5fe3c009d8fe864046e9b
hyoje099 <hyoje099@naver.com>
2022-07-17 18:23:37 +0900

    Create cds.ino

---
+++ cds/cds.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lightValue=analogRead(A0);
+
+  Serial.println(lightValue);
+  analogWrite(3,lightValue/4);
+  delay(1);
+}
