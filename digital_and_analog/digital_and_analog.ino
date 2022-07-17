725df7765417a43f59074d70e1b3c73ea1741d92
hyoje099 <hyoje099@naver.com>
2022-07-17 17:44:16 +0900

    아날로그방식

--- digital_and_analog/digital_and_analog.ino
+++ digital_and_analog/digital_and_analog.ino
@@ -5,5 +5,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  for(int i=0;i<256;i++){
+    analogWrite(5,i);
+    delay(10);
+  }
+  for(int i=255;i>=0;i--){
+    analogWrite(5,i);
+    delay(10);
+  }
 }
