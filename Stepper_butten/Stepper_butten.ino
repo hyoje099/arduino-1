9e3bb1c10ac29aeca28ae4e3c2255c67ea0e8895
hyoje099 <hyoje099@naver.com>
2022-08-07 18:52:12 +0900

    Update Stepper_butten.ino

--- Stepper_butten/Stepper_butten.ino
+++ Stepper_butten/Stepper_butten.ino
@@ -4,19 +4,22 @@ Stepper stepper(stepsPerRev,11,9,10,8);
 void setup() {
   // put your setup code here, to run once:
   stepper.setSpeed(10);
-  pinMode(5,INPUT);
-  pinMode(3,INPUT);
+  pinMode(5,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
   if(digitalRead(3)==HIGH){
-    stepper.step(stepsPerRev);
+    stepper.step(1);
   delay(1000);
+  
   }
   if(digitalRead(5)==HIGH){
-    stepper.step(-stepsPerRev);
-  delay(1000);
+    stepper.step(-1);
+    delay(1000);
+    
 
 
   
