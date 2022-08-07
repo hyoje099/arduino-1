a05dfff5807f326ca439cd325a2e46fd980ecbd8
hyoje099 <hyoje099@naver.com>
2022-08-07 18:21:31 +0900

    Update wifi.ino

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,9 +1,19 @@
+#include<SoftwareSerial.h>
+SoftwareSerial wifi(2,3);
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  wifi.begin(9600);
+  wifi.setTimeout(5000);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available()){
+    wifi.write(Serial.read());
+  }
+  if(Serial.available()){
+    Serial.write(wifi.read());
+  }
 }
